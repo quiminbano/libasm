0eea8ef487264813526a776b23a29e477a1a41d3
Carlos Orellana <corellana1988@gmail.com>
2024-08-12 20:53:30 +0300

    ft_list_push_front donde

--- macOS/ft_list_push_front_bonus.s
+++ macOS/ft_list_push_front_bonus.s
@@ -1,16 +1,16 @@
 section .text
-	global ft_list_push_front
-	extern malloc
+	global _ft_list_push_front
+	extern _malloc
 
-ft_list_push_front:
+_ft_list_push_front:
 	test rsi, rsi
-	jz no_context_push_front
+	jz _no_context_push_front
 	push rdi
 	mov rdi, rsi
-	call ft_create_elem
+	call _ft_create_elem
 	pop rdi
 	test rax, rax
-	jz no_context_push_front
+	jz _no_context_push_front
 	push rsi
 	mov rsi, [rdi]
 	mov [rax + 8], rsi
@@ -20,18 +20,18 @@ ft_list_push_front:
 	ret
 
 
-ft_create_elem:
+_ft_create_elem:
 	push rdi
 	push rsi
 	mov rdi, 16
-	call malloc wrt ..plt
+	call _malloc
 	pop rsi
 	pop rdi
 	test rax, rax
-	jz no_context_push_front
+	jz _no_context_push_front
 	mov [rax], rdi
 	ret
 
-no_context_push_front:
+_no_context_push_front:
 	xor rax, rax
 	ret
