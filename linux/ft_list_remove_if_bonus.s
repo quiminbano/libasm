5012c66ec53cadbb1bb86ac36d4ad5c316df4a03
Carlos Orellana <corellana1988@gmail.com>
2024-08-15 17:21:19 +0300

    removing lines of code from list_remove_if

--- linux/ft_list_remove_if_bonus.s
+++ linux/ft_list_remove_if_bonus.s
@@ -1,8 +1,3 @@
-section .data
-	initial_ptr qword 0
-	current_node qword 0
-	next_node qword 0
-
 section .text
 	global ft_list_remove_if
 	extern free
@@ -16,17 +11,6 @@ ft_list_remove_if:
 	jz return_void_list_remove_if ; Return ; if Function delete pointer is NULL
 	test rdi, rdi
 	jz return_void_list_remove_if ; Return ; if the address of the list is NULL
-	push rdi
-	mov rax, [rdi]
-	pop rdi
-	test rax, rax
-	jz return_void_list_remove_if ; Return ; if the list is empty
-	push rdi
-	mov rdi, rax
-	lea rax, [initial_ptr]
-	mov QWORD[rax], rdi
-
-
 
 return_void_list_remove_if:
 	xor rax, rax
